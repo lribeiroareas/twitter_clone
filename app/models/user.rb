3a8d4c048db2ba70d3c83735d2fcecb33b35e017
Lucas Ribeiro Arêas <42495331+lribeiroareas@users.noreply.github.com>
2018-08-19 15:04:48 -0300

    User follow, unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,24 @@ class User < ApplicationRecord
     has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
     has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
     #source => fonte de onde eu vou obter os dados
-    has_many :followers, through: :active_relationships, source: :followed
-    has_many :followeds, through: :passive_relationships, source: :follower
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 
-    #def following(other_user)
-    	#Este método checa se um usuário está seguindo o outro
-    #end
+    def following?(other_user)
+    	# Este método checa se um usuário está seguindo o outro
+    	following.include? other_user
+    end
 
-   # def follow!(other_user)
-    	#Este método criará o relacionamento
+    def follow!(other_user)
+    	# Este método criará o relacionamento entre um usuário e outro
+    	# following << other_user == following.push other_user (outro método de criar um recionamento)
+    	active_relationships.create(followed: other_user)
+    end
+
+    def unfollow!(other_user)
+    	# Este método apagará o relacionamento entre um usuário e outro 
+    	# following.destroy  other_user idem ao comando de baixo
+    	following.destroy(other_user)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :tweets
  resources :users, only: [:index, :show]
+ resources :relationships, only: [:create, :delete]
 end
